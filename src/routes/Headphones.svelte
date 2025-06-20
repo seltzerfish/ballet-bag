61d5dfc33a6645b545cd96af48743e0902f2dc1c
Conner Lane <seltzerfish@gmail.com>
2025-06-20 13:51:12 -0500

    formatting

--- src/routes/Headphones.svelte
+++ src/routes/Headphones.svelte
@@ -9,17 +9,15 @@
 
 {#if globals.animationPhase === 'bag-exit-pause' || globals.animationPhase === 'bag-exit-components-enter' || globals.animationPhase === 'complete'}
 	<div class="fixed right-0 bottom-0 h-[20rem] w-[20rem]">
-		<div class="relative">
-			<div
-				class:-motion-translate-x-in-100={entering}
-				class:-motion-translate-y-in-[40%]={entering}
-				class:motion-scale-in-0={entering}
-				class="motion-ease-spring-bouncier motion-duration-1000 motion-delay-[1000ms] hue-rotate-15 transition-all hover:hue-rotate-0"
-			>
-				<button onclick={clickHeadphones} class="clickable clickable-rotate p-9">
-					<img src="/headphones.png" alt="headphones" />
-				</button>
-			</div>
+		<div
+			class:-motion-translate-x-in-100={entering}
+			class:-motion-translate-y-in-[40%]={entering}
+			class:motion-scale-in-0={entering}
+			class="motion-ease-spring-bouncier motion-duration-1000 motion-delay-[1000ms] relative hue-rotate-15 transition-all hover:hue-rotate-0"
+		>
+			<button onclick={clickHeadphones} class="clickable clickable-rotate p-9">
+				<img src="/headphones.png" alt="headphones" />
+			</button>
 			<div class="pointer-events-none absolute top-[14rem] -left-[19.5rem] -translate-y-1/2">
 				<h3 class="rounded bg-white px-2 text-2xl">click for ballet music recs</h3>
 			</div>
